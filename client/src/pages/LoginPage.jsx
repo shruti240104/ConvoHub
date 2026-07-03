2d172c3a97e365093d1ee54de18bccd4b04b1d35
shruti240104 <shrutisingh240504@gmail.com>
2026-07-03 19:18:49 +0530

    Add application background image

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
+      
     </div>
   )
 }
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ProfilePage = () => {
   return (
-    <div>
-      <h1>Profile Page</h1>
+    <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
+    
     </div>
   )
 }
