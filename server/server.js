82402d11de2a2763b801a32b7690b609d5a72dfe
shruti240104 <shrutisingh240504@gmail.com>
2026-07-10 17:13:09 +0530

    Connect server to MongoDB

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import "dotenv/config"
 import cors from "cors"
 import http from "http"
+import { connectDB } from './lib/db.js'
 
 //Create Express app using HTTP server
 const app = express();
@@ -12,6 +13,9 @@ app.use(express.json({limit: "4mb"}));
 app.use(cors());
 
 app.use("/api/status",(req,res)=> res.send("Server is live"));
+
+// Connect to mongoDB
+await connectDB()
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT,()=> console.log("Server is running on PORT: "+PORT))
