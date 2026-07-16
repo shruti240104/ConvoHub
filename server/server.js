fc22200f60115eaf3976ac0cd8693ca5871e5783
shruti240104 <shrutisingh240504@gmail.com>
2026-07-16 13:53:29 +0530

    Configure server entry point

--- server/server.js
+++ server/server.js
@@ -5,10 +5,37 @@ import http from "http"
 import { connectDB } from './lib/db.js'
 import userRouter from './routes/userroutes.js'
 import messageRouter from './routes/messageRoutes.js'
+import { Server } from "socket.io"
 
 //Create Express app using HTTP server
 const app = express();
 const server = http.createServer(app)
+
+// Intialize socket.io server
+export const io = new Server(server,{
+  cors: {origin: "*"}
+})
+
+// Store online users
+export const userSocketMap = {} //{userId: socketId}
+
+// Socket.io connection handler
+io.on("connection",(socket) => {
+  const userId = socket.handshake.query.userId
+  console.log("User Connected",userId)
+
+  if(userId) userSocketMap[userId] = socket.id
+
+  // Emit online users to all connected clients
+  io.emit("getOnlineUsers",Object.keys(userSocketMap));
+
+  socket.on("disconnect",()=>{
+    console.log("User Disconnected",userId);
+    delete userSocketMap[userId]
+    io.emit("getOnlineUsers",Object.keys(userSocketMap))
+  })
+
+})
 
 // Middleware setup
 app.use(express.json({limit: "4mb"}));
