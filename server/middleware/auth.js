89368bf7b17d860ce53c6f56d414bba63b0886a0
shruti240104 <shrutisingh240504@gmail.com>
2026-07-17 20:01:49 +0530

    Implement authentication context

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 // Middleware to protect routes
 export const protectRoute = async (req,res,next) => {
   try{
-    const token = requestAnimationFrame.headers.token;
+    const token = req.headers.token;
 
     const decoded = jwt.verify(token,process.env.JWT_SECRET)
 
